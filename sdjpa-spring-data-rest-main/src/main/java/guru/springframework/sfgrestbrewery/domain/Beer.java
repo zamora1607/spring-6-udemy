d73db7e56c5af528e23e9f181d2f521b2e18f7c0
Anna <viima@o2.pl>
2025-09-24 16:15:00 +0200

    Add new project and fix it

---
+++ sdjpa-spring-data-rest-main/src/main/java/guru/springframework/sfgrestbrewery/domain/Beer.java
@@ -0,0 +1,48 @@
+package guru.springframework.sfgrestbrewery.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.*;
+import org.hibernate.type.SqlTypes;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+/**
+ * Created by jt on 2019-05-25.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Beer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @UuidGenerator
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    private UUID id;
+
+    @Version
+    private Long version;
+
+    private String beerName;
+    private BeerStyleEnum beerStyle;
+    private String upc;
+
+    private Integer quantityOnHand;
+    private BigDecimal price;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
+}
